dd83ac853828b86b97b397dda013af8cd7f48fac
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-29 23:10:57 -0500

    created_proyect

---
+++ book_publisher_api/src/main/java/com/example/book_publisher_api/BookPublisherApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_publisher_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPublisherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookPublisherApiApplication.class, args);
+    }
+
+}
---
+++ book_publisher_api/src/test/java/com/example/book_publisher_api/BookPublisherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_publisher_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPublisherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
