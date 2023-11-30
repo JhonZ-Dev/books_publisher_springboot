f78281775d02d09e4b9b42b5f5b7a65d28594b7b
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-29 23:12:44 -0500

    Update Book_jaba.java

--- book_publisher_api/src/main/java/com/example/book_publisher_api/modelo/Book_jaba.java
+++ book_publisher_api/src/main/java/com/example/book_publisher_api/modelo/Book_jaba.java
@@ -8,4 +8,33 @@ public class Book_jaba {
     private String name_jaba;
 
     private List<Publisher_jaba> publishers_jaba;
+    public Long getId_jaba() {
+        return id_jaba;
+    }
+
+    public void setId_jaba(Long id_jaba) {
+        this.id_jaba = id_jaba;
+    }
+
+    public String getName_jaba() {
+        return name_jaba;
+    }
+
+    public void setName_jaba(String name_jaba) {
+        this.name_jaba = name_jaba;
+    }
+
+    public List<Publisher_jaba> getPublishers_jaba() {
+        return publishers_jaba;
+    }
+
+    public void setPublishers_jaba(List<Publisher_jaba> publishers_jaba) {
+        this.publishers_jaba = publishers_jaba;
+    }
+
+    public Book_jaba(Long id_jaba, String name_jaba, List<Publisher_jaba> publishers_jaba) {
+        this.id_jaba = id_jaba;
+        this.name_jaba = name_jaba;
+        this.publishers_jaba = publishers_jaba;
+    }
 }
