412e472d02ad1f185171f81f3ab12f93358d0418
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-29 23:14:48 -0500

    Update Book_jaba_repositorio.java

--- book_publisher_api/src/main/java/com/example/book_publisher_api/repositorio/Book_jaba_repositorio.java
+++ book_publisher_api/src/main/java/com/example/book_publisher_api/repositorio/Book_jaba_repositorio.java
@@ -2,6 +2,10 @@ package com.example.book_publisher_api.repositorio;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class Book_jaba_repositorio {
+    private List<Book_jaba> books_jaba = new ArrayList<>();
 }
