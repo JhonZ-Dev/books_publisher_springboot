83e43cdaf0c8ff211a306369c0dbb575d3e329c8
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-29 23:13:23 -0500

    Update Publisher_jaba.java

--- book_publisher_api/src/main/java/com/example/book_publisher_api/modelo/Publisher_jaba.java
+++ book_publisher_api/src/main/java/com/example/book_publisher_api/modelo/Publisher_jaba.java
@@ -1,4 +1,12 @@
 package com.example.book_publisher_api.modelo;
 
 public class Publisher_jaba {
+    private Long id_publisher_jaba;
+    private String isbn_jaba;
+    private String title_jaba;
+    private Integer total_pages_jaba;
+    private String year_jaba;
+
+    private Book_jaba book;
+
 }
