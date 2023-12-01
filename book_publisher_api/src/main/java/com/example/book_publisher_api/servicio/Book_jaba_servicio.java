3a95c6d38c79fb5d9a916b8657ab5ef66cf19b0d
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-30 22:05:22 -0500

    Update Book_jaba_servicio.java

--- book_publisher_api/src/main/java/com/example/book_publisher_api/servicio/Book_jaba_servicio.java
+++ book_publisher_api/src/main/java/com/example/book_publisher_api/servicio/Book_jaba_servicio.java
@@ -2,7 +2,9 @@ package com.example.book_publisher_api.servicio;
 
 import com.example.book_publisher_api.repositorio.Book_jaba_repositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Book_jaba_servicio {
     @Autowired
     private Book_jaba_repositorio repositorio_jaba;
