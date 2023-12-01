6aaba88176e9669c4e67c811d24fa37a3a32c332
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-30 22:02:17 -0500

    Update Publisher_jaba_repositorio.java

--- book_publisher_api/src/main/java/com/example/book_publisher_api/repositorio/Publisher_jaba_repositorio.java
+++ book_publisher_api/src/main/java/com/example/book_publisher_api/repositorio/Publisher_jaba_repositorio.java
@@ -1,4 +1,11 @@
 package com.example.book_publisher_api.repositorio;
 
+import com.example.book_publisher_api.modelo.Publisher_jaba;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Publisher_jaba_repositorio {
+
+    private List<Publisher_jaba> publishers_jaba = new ArrayList<>();
 }
